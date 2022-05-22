d045f145e06b4e6367030ef898403502a96abee4
Your NameAlex <alehr45@gmail.com>
2022-05-22 17:44:52 -0500

    added routing and page structure

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div style={styles.navStyles}>Navbar</div>;
+};
+
+const styles = {
+  navStyles: {
+    backgroundColor: "lightgrey"
+  }
+};
+
+export default NavBar;
