9cafed7ef3dd82fe91ad6456184ed01aa196a87c
Your NameAlex <alehr45@gmail.com>
2022-06-07 15:53:12 -0500

    changes

---
+++ src/components/BoilerPlate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Component = () => {
+  return <div style={styles.componentStyles}>Component</div>;
+};
+
+const styles = {
+  componentStyles: {
+    backgroundColor: ""
+  }
+};
+
+export default Component;
